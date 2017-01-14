c27085ec151b778b44d0c7f99b3a6de4d328a8e6
GAylabea <gbea@rocketmail.com>
2017-01-13 19:26:59 -0600

    add drop down menu options

--- mainController.js
+++ mainController.js
@@ -1,16 +1,36 @@
 ;(function () {
   angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
     $scope.responses = [];
-    $scope.url = "http://httpstat.us/200";
+    $scope.url ;
+    $scope.methods = [ 'GET', 'POST', 'OPTIONS', 'HEAD' ];
+    $scope.selectMethod = "";
+
+    // try using if/else 
 
     $scope.fetch = () => {
+      // debugger
+      if ($scope.selectMethod == 'GET') {
       requestService.get($scope.url).then((success) => {
         const response = new Response(success);
         $scope.responses.push(response);
       }, (error) => {
-        debugger
-        //do something else
+        console.log("GET error");
       });
+    } else if ($scope.selectMethod == "POST") {
+      requestService.get($scope.url).then((success) => {
+        const response = new Response(success);
+        $scope.responses.push(response);
+      }, (error) => {
+        console.log("POST error");
+      });      
+    } else if ($scope.selectMethod == "HEAD") {
+      requestService.get($scope.url).then ((success) => {
+        const response = new Response(success);
+        $scope.response.push(response);
+      }, (error) => {
+        console.log("HEAD error");
+      });
+    }
     };
   });
 })();
--- requestService.js
+++ requestService.js
@@ -1,5 +1,10 @@
 ;(function () {
   angular.module('fetchApp').service('requestService', function ($http) {
+    // if (requestService == 'GET') {
     this.get = (url) => $http.get(url);
+  // } else if (requestService == 'POST')
+    // will need to add post, options and head
+    // this.get = (url) => $http.post(url);
+    // console.log("POST");
   });
 })();
